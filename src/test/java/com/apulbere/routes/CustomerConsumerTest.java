0c263cabacec4cda4258ca05404c406fbec12d7f
Adrian Pulbere <apulbere@yahoo.com>
2018-01-03 00:39:31 +0200

    add 'mock' for redis

--- src/test/java/com/apulbere/routes/CustomerConsumerTest.java
+++ src/test/java/com/apulbere/routes/CustomerConsumerTest.java
@@ -4,8 +4,12 @@ import com.apulbere.cprocessor.routes.CustomerConsumer;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.model.ToDynamicDefinition;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +34,25 @@ public class CustomerConsumerTest extends CamelTestSupport {
         return camelContext;
     }
 
+    @Before
+    public void init() throws Exception {
+        camelContext.getRouteDefinitions().get(0).adviceWith(context,
+            new AdviceWithRouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    weaveByType(ToDynamicDefinition.class).replace().to("mock:toRedis");
+                }
+        });
+    }
+
     @Test
     public void notWorkingTest() throws Exception {
+        MockEndpoint mockToRedis = getMockEndpoint("mock:toRedis");
+        mockToRedis.expectedMessageCount(2);
+
         producerTemplate.sendBody("8f9ed0f5-1440-4754-954a-4ef4da1a2093");
+
+        mockToRedis.assertIsSatisfied();
     }
 
 
